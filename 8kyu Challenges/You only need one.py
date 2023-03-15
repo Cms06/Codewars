9aff7b6872052e82915fc4f187a48f8c7d07491d
Cms06 <119540462+Cms06@users.noreply.github.com>
2023-03-15 16:18:32 +0800

    Solution

---
+++ 8kyu Challenges/You only need one.py
@@ -0,0 +1,22 @@
+'''
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+
+'''
+def check(seq, elem):
+    for i in seq:
+        if i == elem:
+            return True
+    return False
+    
+
+'''
+Test    
+(True, ([66, 101], 66)),
+(False, ([78, 117, 110, 99, 104, 117, 107, 115], 8)),
+(True, ([101, 45, 75, 105, 99, 107], 107)),
+(True, ([80, 117, 115, 104, 45, 85, 112, 115], 45))
+'''
